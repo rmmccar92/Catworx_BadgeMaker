3b84b792af33c23fba8a2ec13c2068a26f708cd0
Ryan McCarthy <rmmcc92@aol.com>
2021-12-16 21:16:58 -0500

    created Utils.cs to write the employees.csv file

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+// Demo URL https://placekitten.com/300/300
+
 namespace CatWorx.BadgeMaker
 {
     class Program
@@ -29,18 +31,12 @@ namespace CatWorx.BadgeMaker
             }
             return employees;
         }
-        static void PrintEmployees(List<Employee> employees)
-        {
-            for (int i = 0; i < employees.Count; i++)
-            {
-                string template = "{0,-10}\t{1,-20}\t{2}";
-                Console.WriteLine(String.Format(template, employees[i].GetId(), employees[i].GetName(), employees[i].GetPhotoUrl()));
-            }
-        }
+
         static void Main(string[] args)
         {
             List<Employee> employees = GetEmployees();
-            PrintEmployees(employees);
+            Util.PrintEmployees(employees);
+            Util.MakeCSV(employees);
         }
     }
 }
