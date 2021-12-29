1c56d91b307df1c4127b59b927d6052fd8145a9a
Ryan McCarthy <rmmcc92@aol.com>
2021-12-29 16:18:47 -0500

    badges now add cat picture

--- Program.cs
+++ Program.cs
@@ -37,6 +37,7 @@ namespace CatWorx.BadgeMaker
             List<Employee> employees = GetEmployees();
             Util.PrintEmployees(employees);
             Util.MakeCSV(employees);
+            Util.MakeBadges(employees);
         }
     }
 }
--- Util.cs
+++ Util.cs
@@ -2,6 +2,8 @@
 // using System;
 // using System.Collections.Generic;
 // using System.IO;
+using System.Drawing;
+using System.Net;
 namespace CatWorx.BadgeMaker
 {
     class Util
@@ -33,6 +35,56 @@ namespace CatWorx.BadgeMaker
                     file.WriteLine(String.Format(template, employees[i].GetId(), employees[i].GetName(), employees[i].GetPhotoUrl()));
                 }
             }
+        }
+
+        // Make Badges function
+        public static void MakeBadges(List<Employee> employees)
+        {
+            // Layout variables
+            int BADGE_WIDTH = 669;
+            int BADGE_HEIGHT = 1044;
+
+            int COMPANY_NAME_START_X = 0;
+            int COMPANY_NAME_START_Y = 110;
+            int COMPANY_NAME_WIDTH = 100;
+
+            int PHOTO_START_X = 184;
+            int PHOTO_START_Y = 215;
+            int PHOTO_WIDTH = 302;
+            int PHOTO_HEIGHT = 302;
+
+            int EMPLOYEE_NAME_START_X = 0;
+            int EMPLOYEE_NAME_START_Y = 560;
+            int EMPLOYEE_NAME_WIDTH = BADGE_WIDTH;
+            int EMPLOYEE_NAME_HEIGHT = 100;
+
+            int EMPLOYEE_ID_START_X = 0;
+            int EMPLOYEE_ID_START_Y = 690;
+            int EMPLOYEE_ID_WIDTH = BADGE_WIDTH;
+            int EMPLOYEE_ID_HEIGHT = 100;
+
+
+
+            // instance of WebClient is disposed after code in the block has run
+            using (WebClient client = new WebClient())
+            {
+                for (int i = 0; i < employees.Count; i++)
+                {
+                    // Stream employeeStream = client.OpenRead(employees[i].GetPhotoUrl());
+                    // Image photo = Image.FromStream(employeeStream);
+                    // Combines the two previous lines
+                    Image photo = Image.FromStream(client.OpenRead(employees[i].GetPhotoUrl()));
+                    Image background = Image.FromFile("badge.png");
+                    Image badge = new Bitmap(BADGE_WIDTH, BADGE_HEIGHT);
+                    Graphics graphic = Graphics.FromImage(badge);
+                    graphic.DrawImage(background, new Rectangle(0, 0, BADGE_WIDTH, BADGE_HEIGHT));
+                    graphic.DrawImage(photo, new Rectangle(PHOTO_START_X, PHOTO_START_Y, PHOTO_WIDTH, PHOTO_HEIGHT));
+                    badge.Save("data/employeeBadge.png");
+                    // background.Save("data/employeeBadge" + employees[i].GetId() + ".png");
+                }
+            }
+            // Image newImage = Image.FromFile("badge.png");
+            // newImage.Save("data/employeeBadge.png");
         }
     }
 }
